cc58cb7f32632f4337ae871aa3463c73a682f79b
SH20RAJ <shaswatraj3@gmail.com>
2025-12-30 19:20:29 +0530

    fix: disable setImmediate polyfill to resolve Cloudflare deployment error

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
 import type { NextConfig } from "next";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
+// Enable calling `getCloudflareContext()` in `next dev`.
+// See https://opennext.js.org/cloudflare/bindings#local-access-to-bindings.
+initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
-	/* config options here */
-};
-
-export default {
-	...nextConfig,
+	webpack: (config) => {
+		config.resolve.alias = {
+			...config.resolve.alias,
+			// Prevent Next.js from trying to patch setImmediate which is read-only in CF Workers
+			'next/dist/server/node-environment-extensions/fast-set-immediate': false,
+		};
+		return config;
+	},
 	images: {
 		remotePatterns: [
 			{
@@ -16,7 +24,4 @@ export default {
 	},
 };
 
-// Enable calling `getCloudflareContext()` in `next dev`.
-// See https://opennext.js.org/cloudflare/bindings#local-access-to-bindings.
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev();
+export default nextConfig;
